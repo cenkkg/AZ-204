09e17d4dd6908980b62d6f8ef8b1eb064696b771
cenkgurbet <cenk.gurbet@deepnetwork.com>
2023-07-03 17:39:50 +0200

    CG - EXERCISE - RUBY Azure Blob Uploader - 1

---
+++ EXERCISE/Ruby/azureBlobStorageLoader.rb
@@ -0,0 +1,24 @@
+require 'azure/storage/blob'
+
+def getInputFromUser(message)
+  print message
+  gets.chomp
+end
+
+
+# Replace with your Azure storage account name and access key
+account_name = ENV['ACCOUNT_NAME']
+access_key = ENV['ACCOUNT_KEY']
+container_name = get("Enter your container name: ")
+file_path = get("Enter your file name (With full path): ")
+
+# Create a blob service client
+blob_client = Azure::Storage::Blob::BlobService.create(
+  storage_account_name: account_name,
+  storage_access_key: access_key
+)
+
+blob_name = File.basename(file_path)
+blob_client.create_block_blob(container_name, blob_name, File.open(file_path, 'rb'))
+
+
